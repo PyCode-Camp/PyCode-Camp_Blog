5965a8230a51e85b66a28067808e9973fcda964f
s-kvng <kvngnathan8420@gmail.com>
2023-03-08 10:08:18 +0000

    Update structure

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,26 +2,31 @@
 <html>
 <head>
 <?php include 'includes\head.php';?>
+@include('admin.includes.head')
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
 
   <header class="main-header">
   <?php include 'includes\header.php';?>
+  @include('admin.includes.header')
   </header>
   <!-- Left side column. contains the logo and sidebar -->
   <aside class="main-sidebar">
   <?php include 'includes\sidebar.php';?>
+  @include('admin.includes.sidebar')
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
   <?php include 'includes\control_dashbord.php';?>
+  @include('admin.includes.control_dashboard')
   </div>
  
 
   <footer class="main-footer">
   <?php include 'includes\footer.php';?>
+  
   </footer>
 
 
