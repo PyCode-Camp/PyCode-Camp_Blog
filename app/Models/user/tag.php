7e4b9c76e3f8c4b008aaf0c8f3546fa4034c4930
s-kvng <kvngnathan8420@gmail.com>
2023-03-13 00:07:39 +0000

    Update Relationships
    Add many to many relation btw posts and tag & posts with category

--- app/Models/user/tag.php
+++ app/Models/user/tag.php
@@ -4,8 +4,16 @@ namespace App\Models\user;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class tag extends Model
 {
     use HasFactory;
+
+
+    public function posts(): BelongsToMany 
+    {
+
+        return $this->belongsToMany('App\Models\user\Post', 'post_tags');
+    }
 }
