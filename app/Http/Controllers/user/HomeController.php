24a68842e561ab03a2e2bb5fefa0b0b72131d52c
s-kvng <kvngnathan8420@gmail.com>
2023-03-13 21:01:27 +0000

    Update post page/ homeController

--- app/Http/Controllers/user/HomeController.php
+++ app/Http/Controllers/user/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\user;
 
+use App\Models\user\category;
 use App\Models\user\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -12,7 +13,14 @@ class HomeController extends Controller
 
     public function index(){
 
-        $posts = Post::all()->sortByDesc('created_at');
-        return view('user.home', compact('posts'));
+        $posts = Post::where('status', 1)->get()->sortByDesc('created_at');
+        
+        $trendingCategory = category::where('slug', 'trending')->get()->first();
+
+        $trendCategories =  $trendingCategory->posts->where('status', 1)->sortByDesc('created_at');
+        
+        
+
+        return view('user.home', compact('posts', 'trendCategories'));
     }
 }
--- resources/views/user/main-post.blade.php
+++ resources/views/user/main-post.blade.php
@@ -2,20 +2,35 @@
 
 
 @section('mainContent')
-    <section class="text-gray-600 body-font mb-10">
+    <section class="text-gray-600 body-font mb-20">
         <div class="container mx-auto px-4">
 
             <div class="container mx-auto flex flex-col justify-center items-center">
                 <img
-                    class=" object-cover object-center md:w-56 md:h-32 lg:w-auto lg:h-60 rounded"src="{{ asset('user/assets/img/Car 14.jpg') }}">
+                    class=" object-cover object-center md:w-56 md:h-32 lg:w-auto lg:h-80 rounded"src="{{ asset('user/assets/img/Car 14.jpg') }}">
             </div>
             <br>
 
-            <h1 class="text-2xl font-bold text-center mb-7">{{ $post->title }}</h1>
-            <p class="mb-5">
-             {!! htmlspecialchars_decode($post->body) !!}
+            <h1 class="text-3xl font-bold text-center mb-1">{{ $post->title }}</h1>
+            <p class="   mb-1"> <small>created at {{ $post->created_at->diffForHumans() }}</small></p>
+            <p class="text-sm text-yellow-700 me-4 mb-5">
+                @foreach ($post->categories as $postCategory )
+                    {{ $postCategory['name']}} ,
+                @endforeach
             </p>
+            
+             <div class="mb-2">
+                {!! htmlspecialchars_decode($post->body) !!}
+             </div>
+            
+             
 
+             {{-- tag clouds --}}
+            <div class=" text-right">
+                @foreach ($post->tags as $postTag )
+                {{ $postTag['name']}} ,
+               @endforeach
+            </div>
 
     </section>
 @endsection
