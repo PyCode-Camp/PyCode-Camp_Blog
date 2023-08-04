c74c76e07274104d5c5a9e7d3d4b57e5bfd44a9d
nathan <kvngnathan8420@gmail.com>
2023-08-04 03:39:46 +0000

    add next & prev feature

--- app/Http/Controllers/user/UserPostController.php
+++ app/Http/Controllers/user/UserPostController.php
@@ -5,13 +5,29 @@ namespace App\Http\Controllers\user;
 use App\Http\Controllers\Controller;
 use App\Models\user\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class UserPostController extends Controller
 {
     
     public function post(Post $post){
 
+        $next = Post::query()
+            ->where('status' , 1)
+            ->whereDate('created_at', '<=' , Carbon::now())
+            ->whereDate('created_at', '<', $post->created_at)
+            ->orderBy('created_at', 'desc')
+            ->limit(value: 1)
+            ->first();
+
+        $prev = Post::query()
+            ->where('status' , 1)
+            ->whereDate('created_at', '<=' , Carbon::now())
+            ->whereDate('created_at', '>', $post->created_at)
+            ->orderBy('created_at', 'asc')
+            ->limit(value: 1)
+            ->first();
         
-        return view('user.main-post',compact('post'));
+        return view('user.main-post',compact('post', 'next' , 'prev'));
     }
 }
--- resources/views/user/main-post.blade.php
+++ resources/views/user/main-post.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('mainContent')
-    <section class="text-gray-600 body-font mb-20">
+    <section class="text-gray-600 body-font mb-16">
         <div class="container mx-auto px-4">
 
             <div class="container mx-auto flex flex-col justify-center items-center">
@@ -32,5 +32,25 @@
                @endforeach
             </div>
 
+                <div class=" flex justify-between items-center gap-2 mt-2">
+                <div class=" w-1/2">
+                @if($prev)
+                        <a href="{{ route('post', $prev->slug)}}" class=" flex flex-col shadow-xl hover:shadow-2xl px-2 h-[82px] min-h-[82px] max-h-[82px] bg-white">
+                                <p class=" text-lg text-blue-700 font-bold flex items-center"><ion-icon name="arrow-back-outline" class=" mr-2"></ion-icon> Previous</p>
+                                <p> {{ Str::limit($prev->title, 30) }}</p>
+                        </a>
+                    @endif
+                </div>
+
+                    <div class="w-1/2">
+                    @if($next)
+                        <a href="{{ route('post', $next->slug)}}"  class=" flex flex-col shadow-xl hover:shadow-2xl px-2 h-[82px] min-h-[82px] max-h-[82px] bg-white">
+                        <p class=" text-lg text-blue-700 font-bold flex items-center ">  Next <ion-icon name="arrow-forward-outline" class="ml-2"></ion-icon> </p>
+                            <p>  {{ Str::limit($next->title, 30) }}</p>
+                        </a>
+                    @endif
+                    </div>
+                </div>
     </section>
+
 @endsection
